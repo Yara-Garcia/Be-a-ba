bfe66b5e0feed303f59b1e5169f94411dc79c21e
seu_nome_de_usuario <y.coelho@yahoo.com>
2024-06-11 17:39:44 -0300

    atualização do ID do campo nome em editarModulo.html

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ const PORT = 3000;
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080'); // Permite acesso do cliente em http://localhost:8080
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
 // Middleware para parsear o corpo das requisições em JSON
 app.use(bodyParser.json());
 
