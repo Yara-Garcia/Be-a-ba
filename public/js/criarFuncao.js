786ae6c42ad27b987f6b59be0376638d8e621d8f
seu_nome_de_usuario <y.coelho@yahoo.com>
2024-06-14 14:07:21 -0300

    script para criar função desenvolvido

---
+++ public/js/criarFuncao.js
@@ -0,0 +1,42 @@
+document.getElementById('btn-cancelar').addEventListener('click', function () {
+    window.location.href = '../html/gestaoFuncoes.html';
+});
+
+document.getElementById('form-container').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nomeFuncao = document.getElementById('nome-funcao').value;
+    const descricao = document.getElementById('descricao').value;
+
+    const dadosFuncao = {
+        nome_funcao: nomeFuncao,
+        descricao: descricao
+    };
+
+    fetch('http://localhost:3000/function', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosFuncao)
+    })
+        .then(response => {
+            if (!response.ok) {
+                const error = response.statusText;
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                alert('Função criada com sucesso!');
+                window.location.href = '../html/gestaoFuncoes.html';
+            } else {
+                console.log('Data:', data.message)
+                alert('Falha na criação: ' + data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Falha na criação: ' + error.message);
+        });
+});
