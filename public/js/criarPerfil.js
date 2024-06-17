1647cac09c092e80b967201a576ca146775aee79
seu_nome_de_usuario <y.coelho@yahoo.com>
2024-06-17 15:29:20 -0300

    script criado para carregar os módulos, transações dependentes e funções para criação de perfil

--- public/js/criarPerfil.js
+++ public/js/criarPerfil.js
@@ -30,202 +30,174 @@ window.addEventListener('click', function (event) {
     }
 });
 
-fetch('http://localhost:3000/modules', {
-    method: 'GET'
-})
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Falha na requisição: ' + response.statusText);
-        }
 
-        return response.json();
-    })
-
-    .then(data => {
-        if (data.success) {
-            const modules = data.modules;
-
-            const dropdownModules = document.getElementById('dropdownModules');
-            const dropdownContent = dropdownModules.querySelector('.dropdown-content');
-
-            if (dropdownModules) {
+// Função para buscar e popular os módulos no dropdown de seleção de módulos
+function fetchModules() {
+    fetch('http://localhost:3000/modules')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Falha na requisição: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                const modules = data.modules;
+                const dropdownContent = document.getElementById('dropdownModulesContent');
                 dropdownContent.innerHTML = '';
 
                 modules.forEach(module => {
                     const label = document.createElement('label');
-
-                    const input = document.createElement('input');
-                    input.className = 'checkbox';
-                    input.type = 'checkbox';
-                    input.value = module.id_modulo;
-
-                    const textNode = document.createTextNode(module.nome_modulo);
-
-                    label.appendChild(input);
-                    label.appendChild(textNode);
-
+                    label.innerHTML = `
+                        <input type="checkbox" class="module-checkbox" value="${module.id_modulo}">
+                        ${module.nome_modulo}
+                    `;
                     dropdownContent.appendChild(label);
                 });
-            }
-        } else {
-            console.error('Erro ao buscar módulos:', data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-    });
 
-
-document.querySelectorAll('#dropdownModules .checkbox').forEach(input => {
-    input.addEventListener('change', function (event) {
-        event.preventDefault();
-        const modulosSelecionados = Array.from(document.querySelectorAll('#dropdownModules .checkbox:checked')).map(el => el.value);
-        console.log(modulosSelecionados)
-
-        fetch('http://localhost:3000/moduleTransactionAssociationsList', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
+                // Adicionar listener para selecionar módulos
+                document.querySelectorAll('.module-checkbox').forEach(input => {
+                    input.addEventListener('change', function (event) {
+                        event.preventDefault();
+                        const selectedModules = Array.from(document.querySelectorAll('.module-checkbox:checked')).map(el => el.value);
+                        fetchAssociatedTransactions(selectedModules);
+                    });
+                });
+            } else {
+                console.error('Erro ao buscar módulos:', data.message);
             }
         })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Falha na requisição: ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.success) {
-                    const transacoesAssociadas = new Set();
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
 
-                    data.associations.forEach(association => {
-                        if (modulosSelecionados.includes(association.id_modulo)) {
-                            transacoesAssociadas.add(association.id_transacao);
+// Função para buscar transações associadas aos módulos selecionados
+function fetchAssociatedTransactions(selectedModules) {
+    console.log(selectedModules);
+    fetch('http://localhost:3000/moduleTransactionAssociationsList')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Falha na requisição: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+
+                const associatedTransactions = new Set();
+
+                data.associations.forEach(association => {
+                    const moduloString = association.id_modulo.toString();
+
+                    if (selectedModules.includes(moduloString)) {
+                        associatedTransactions.add(association.id_transacao);
+                    }
+                });
+
+                fetch('http://localhost:3000/transactions')
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Falha na requisição: ' + response.statusText);
                         }
-                    });
-
-                    fetch('http://localhost:3000/transactions', {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
+                        return response.json();
                     })
-                        .then(response => {
-                            if (!response.ok) {
-                                throw new Error('Falha na requisição: ' + response.statusText);
-                            }
-                            return response.json();
-                        })
-                        .then(data => {
-                            if (data.success) {
-                                const transactions = data.transactions.filter(transaction => transacoesAssociadas.has(transaction.id_transacao));
-                                updateTransactionsDropdown(transactions);
-                            } else {
-                                console.error('Erro ao buscar transações:', data.message);
-                            }
-                        })
-                        .catch(error => {
-                            console.error('Erro:', error);
-                        });
-                } else {
-                    console.error('Erro ao buscar IDs de transações:', data.message);
-                }
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-            });
-    });
-});
+                    .then(data => {
+                        if (data.success) {
+                            const transactions = data.transactions.filter(transaction => associatedTransactions.has(transaction.id_transacao));
+                            updateTransactionsDropdown(transactions);
+                        } else {
+                            console.error('Erro ao buscar transações:', data.message);
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erro:', error);
+                    });
+            } else {
+                console.error('Erro ao buscar associações de módulo/transação:', data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
 
+// Função para atualizar o dropdown de transações com as transações filtradas
 function updateTransactionsDropdown(transactions) {
-    const transactionDropdown = document.querySelector('#transactionsDropdown .dropdown-content');
+    const transactionDropdown = document.getElementById('transactionsDropdownContent');
     transactionDropdown.innerHTML = '';
 
     transactions.forEach(transaction => {
-        console.log(transactions)
         const label = document.createElement('label');
-        label.className = 'dropdown-item';
-        label.innerHTML = `<input type="checkbox" value="${transaction.id_transacao}">${transaction.nome_transacao}`;
+        label.innerHTML = `
+            <input type="checkbox" class="transaction-checkbox" value="${transaction.id_transacao}">
+            ${transaction.nome_transacao}
+        `;
         transactionDropdown.appendChild(label);
     });
-    // Mostrar o dropdown de transações
+
     document.getElementById('transactionsDropdown').style.display = transactions.length ? 'block' : 'none';
+
+    // Adicionar listener para abrir o modal com funções associadas à transação
+    document.querySelectorAll('.transaction-checkbox').forEach(input => {
+        input.addEventListener('change', function (event) {
+            event.preventDefault();
+            if (this.checked) {
+                const transactionId = this.value;
+                openFunctionsModal(transactionId);
+            }
+        });
+    });
 }
 
-document.getElementById('btn-cancelar').addEventListener('click', function () {
-    window.location.href = '../html/gestaoTransacoes.html';
-});
-
-document.getElementById('btn-salvar').addEventListener('click', function (event) {
-    event.preventDefault();
-    //falta editar
-});
-
-
-/*function updateTransactionsDropdown(transactions) {
-    const transactionDropdown = document.querySelector('#transactionsDropdown .dropdown-content');
-    // Limpar o dropdown de transações
-    transactionDropdown.innerHTML = '';
-    // Adicionar as transações ao dropdown
-    transactions.forEach(transaction => {
-        const label = document.createElement('label');
-        label.className = 'dropdown-item';
-        label.innerHTML = `<input type="checkbox" value="${transaction.id}">${transaction.nome}`;
-        transactionDropdown.appendChild(label);
-    });
-    // Mostrar o dropdown de transações
-    document.getElementById('transactionsDropdown').style.display = transactions.length ? 'block' : 'none';
-}
-
-
-document.querySelectorAll('#dropdownModules .checkbox').forEach(input => {
-    input.addEventListener('change', function () {
-        const selectedModules = Array.from(document.querySelectorAll('#dropdownModules .checkbox:checked')).map(el => el.value);
-        const transactionDropdown = document.querySelector('#transactionsDropdown .dropdown-content');
-
-
-        const searchInput = document.createElement('input');
-        searchInput.type = 'text';
-        searchInput.className = 'dropdown-search';
-        searchInput.placeholder = 'Buscar...';
-
-
-        while (transactionDropdown.firstChild) {
-            transactionDropdown.removeChild(transactionDropdown.firstChild);
-        }
-
-        transactionDropdown.appendChild(searchInput);
-
-        let transactionItems = new Set();
-        selectedModules.forEach(module => {
-            modules[module].forEach(transaction => {
-                transactionItems.add(transaction);
-            });
-        });
-
-        transactionItems.forEach(transaction => {
-            const label = document.createElement('label');
-            label.className = 'dropdown-item';
-            label.innerHTML = `<input type="checkbox" value="${transaction}">${transaction}`;
-            label.querySelector('input').addEventListener('change', function () {
-                if (this.checked) {
-                    openFunctionsModal(transaction);
-                }
-            });
-            transactionDropdown.appendChild(label);
-        });
-        document.getElementById('transactionsDropdown').style.display = selectedModules.length ? 'block' : 'none';
-    });
-});
-
-function openFunctionsModal(transaction) {
+//Função para abrir o modal com funções associadas à transação selecionada
+function openFunctionsModal(transactionId) {
     const modalBody = document.querySelector('#functionsModal .modal-body');
     modalBody.innerHTML = '';
-    transactions[transaction].forEach(func => {
-        const label = document.createElement('label');
-        label.className = 'dropdown-item';
-        label.innerHTML = `<input type="checkbox" value="${func}">${func}`;
-        modalBody.appendChild(label);
-    });
+
+    fetch('http://localhost:3000/functions')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Falha na requisição: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+
+                const functionsList = data.functions;
+                functionsList.forEach(func => {
+
+                    const label = document.createElement('label');
+                    label.className = 'dropdown-item';
+                    label.innerHTML = `<input type="checkbox" value="${func.id_funcao}">
+                    ${func.nome_funcao}`;
+                    modalBody.appendChild(label);
+                })
+            } else {
+                console.error('Erro ao buscar funções:', data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+
     $('#functionsModal').modal('show');
-}*/
+    console.log('Abrir modal com funções para a transação:', transactionId);
+}
+
+// Event listener para o botão cancelar
+document.getElementById('btn-cancelar').addEventListener('click', function () {
+    window.location.href = '../html/gestaoPerfis.html';
+});
+
+// Event listener para o botão salvar
+document.getElementById('btn-salvar').addEventListener('click', function (event) {
+    event.preventDefault();
+    // Implemente a lógica de salvar as transações selecionadas, se necessário
+    console.log('Implemente a lógica de salvar as transações selecionadas');
+});
+
+// Inicialização da página
+fetchModules();
+
